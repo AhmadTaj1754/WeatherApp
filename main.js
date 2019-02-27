6af37f1efee081b209a7ba1efa093b435182f570
Ahmad Tajgardoun <ahmadtaj@Ahmads-MBP.fios-router.home>
2019-02-27 12:53:24 -0500

    Added geolocation and cleaned up AJAX code

--- main.js
+++ main.js
@@ -1,79 +1,45 @@
-
-
-
-function loadDoc()
-{
-
-
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function()
-  {
-    if (this.readyState == 4 && this.status == 200)
-    {
-      var response = this.responseText;
-
-       var myObj = JSON.parse(this.responseText);
-
-
-
-      var arrWeatherText= myObj.weather;
-
-      document.getElementById("weathertext").innerHTML =
-      arrWeatherText[0].main;
-
-      document.getElementById("weatherdesc").innerHTML =
-      arrWeatherText[0].description;
-
-
-
-      document.getElementById("ico").src=
-       arrWeatherText[0].icon;
-
-      var arrMain= myObj.main;
-
-      //round degree
-      var degreeRound =  Math.round(arrMain.temp);
-
-      document.getElementById("tempcel").innerHTML=
-       degreeRound + " ºC";
-
-
-
-
-        document.getElementById("info").innerHTML =
-      response;
-
-
-    }
-  };
-
-   // + lat + "&" + lon
-  xhttp.open("GET", "https://fcc-weather-api.glitch.me/api/current?lat=42.418560&lon=-71.106450", true);
-  xhttp.send();
-}
-
-
-
-function weatherFetcher() {
-
-
-
 var api = "https://fcc-weather-api.glitch.me/api/current?";
 var lat, lon;
 var tempUnit = 'C';
 var currentTempInCelsius;
 
-  var urlString = api + "lat=42.418560" + "&" + "lon=-71.106450";
+function getLocation()
+{
+  if (navigator.geolocation)
+  {
+    navigator.geolocation.getCurrentPosition(showPosition);
+
+
+  }
+  else
+  {
+    document.getElementById("error").innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+}
+
+function weatherFetcher() {
+
+  // var urlString = api + "lat=42.418560" + "&" + "lon=-71.106450";
+  getLocation();
+   var urlString = api + "lat=" + lat + "&" + "lon=" + lon;
+
   $.ajax({
     url: urlString,
     success: function (result)
     {
      $("#weathertext").text(result.weather[0].main);
      $("#weatherdesc").text(result.weather[0].description);
-     $("#tempcel").text(Math.round(result.main.temp));
+     $("#tempcel").text(Math.round(result.main.temp) + "º C");
       var image = result.weather[0].icon;
       console.log(image);
       $("#ico").append("<img src= '"+image+"' >");
+      $("#alerting").text("Hello " + result.name + "!");
 
 
      $("#info").text(JSON.stringify(result));
