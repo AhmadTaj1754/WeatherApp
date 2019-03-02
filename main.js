2dd52335210595ffc4c2e36c09c894e4aafe5e98
Ahmad Tajgardoun <ahmadtaj@Ahmads-MBP.fios-router.home>
2019-03-02 08:39:49 -0500

    Added Celsius to Farenheit conversion functionality

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 var api = "https://fcc-weather-api.glitch.me/api/current?";
 var lat, lon;
 var tempUnit = 'C';
-var currentTempInCelsius;
+var TempInCelsius;
+var TempInCelsiusLow;
+var TempInCelsiusHigh;
+var TempInFarenheit;
+var TempInFarenheitLow;
+var TempInFarenheitHigh;
+var IsFar = false;
 
 function getLocation()
 {
@@ -23,37 +29,85 @@ function showPosition(position) {
     lon = position.coords.longitude;
 }
 
-function weatherFetcher() {
+function weatherFetcher()
+{
 
   // var urlString = api + "lat=42.418560" + "&" + "lon=-71.106450";
   getLocation();
    var urlString = api + "lat=" + lat + "&" + "lon=" + lon;
 
-  $.ajax({
+  $.ajax
+({
     url: urlString,
     success: function (result)
     {
+
+     TempInCelsius = result.main.temp;
+
      $("#weathertext").text(result.weather[0].main);
      $("#weatherdesc").text(result.weather[0].description);
-     $("#tempcel").text(Math.round(result.main.temp) + "º C");
+     $("#temp").text(Math.round(result.main.temp) + "º C");
       var image = result.weather[0].icon;
       console.log(image);
       $("#ico").append("<img src= '"+image+"' >");
       $("#alerting").text("Hello " + result.name + "!");
 
 //       Details
+      TempInCelsiusLow = result.main.temp_min;
+      TempInCelsiusHigh = result.main.temp_max;
+
+
+
       $("#pressure").text("Pressure "+ result.main.pressure);
       $("#humidity").text("Humidity "+ result.main.humidity);
       $("#temp_min").text("Low "+ Math.round(result.main.temp_min)+ "º C");
-      $("#temp_max").text("High "+ Math.round(result.main.temp_max) +"º C");
+      $("#temp_max").text("High "+ Math.round(result.main.temp_max) +"º             C");
       $("#visibility").text("Visibility "+ result.visibility);
       $("#windspeed").text("Wind Speed "+ result.wind.speed);
 
       // $("#winddegree").text("Wind Degree "+ result.wind.deg);
-      $("#sunrise").text("Sunrise "+ result.sys.sunrise);
-      $("#sunset").text("Sunset "+ result.sys.sunset);
+
+      //convert  date/ time  -- result.sys.sunrise
+
+//       $("#sunrise").text("Sunrise "+ result.sys.sunrise );
+//       $("#sunset").text("Sunset "+ result.sys.sunset);
 
 
     }
   });
+}
+
+function convertToFar()
+{
+  if(IsFar)
+  {
+    TempInCelsius =  Math.round((TempInFarenheit - 32) * (5/9));
+    TempInCelsiusLow = Math.round((TempInFarenheitLow - 32) * (5/9));
+    TempInCelsiusHigh = Math.round((TempInFarenheitHigh - 32) * (5/9));
+
+    document.getElementById("temp").innerHTML = TempInCelsius + "º C";
+    document.getElementById("temp_min").innerHTML = TempInCelsiusLow + "º C";
+    document.getElementById("temp_max").innerHTML = TempInCelsiusHigh + "º C";
+
+
+    IsFar= false;
+  }
+  else
+    {
+
+      TempInFarenheit =  Math.round((TempInCelsius * (9/5)) + 32);
+      TempInFarenheitLow =  Math.round((TempInCelsiusLow * (9/5)) + 32);
+      TempInFarenheitHigh =  Math.round((TempInCelsiusHigh * (9/5)) + 32);
+
+      document.getElementById("temp").innerHTML = TempInFarenheit + "º F";
+      document.getElementById("temp_min").innerHTML = TempInFarenheitLow + "º F";
+      document.getElementById("temp_max").innerHTML = TempInFarenheitHigh + "º F";
+
+
+
+
+      IsFar = true;
+    }
+
+
 }
